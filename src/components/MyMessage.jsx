f4ba69004c2f18a5e06e1cd0e67c86f36f57346f
Nick Tse <nicholashtse@gmail.com>
2023-03-21 10:11:49 -0400

    build MyMessage and MessageForm components

---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function MyMessage({ message }) {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message attachment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: 18,
+        color: "white",
+        backgroundColor: "#ccc",
+      }}
+    >
+      {message.text}
+    </div>
+  );
+}
